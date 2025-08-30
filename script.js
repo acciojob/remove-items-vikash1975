f5470923289c719fd6c22ac2ef223ccb8456c3b5
vikash1975 <167326024+vikash1975@users.noreply.github.com>
2025-08-30 20:21:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+
+let select=document.getElementById("colorSelect");
+let btn=document.getElementById("removebtn");
+
+btn.addEventListener("click",function(){
+let selectedIndex=select.selectedIndex;
+
+	if(selectedIndex !== -1){
+		select.remove(selectedIndex);
+	}
+	});
